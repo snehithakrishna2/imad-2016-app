a6647e9d6ad1c4283662fa8d8ae9e5e86dd95fe1
snehithakrishna2 <snehithakrishna7@gmail.com>
2016-11-24 08:16:18 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+  user: 'snehithakrishna2',
+  database:'snehithakrishna2',
+  host: 'db.imad.hasura-app.io',
+  port:'5432',
+  password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -8,6 +16,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+ //make a request
+ // returns a response from a results
+ Pool.query('SELECT * FROM test',function(err,result){
+ if(err){
+     res.status(500).send(err.toString());
+ }else{
+     res.send(JSON.stringify(result));
+ }
+   });
+ });
 app.get('/about', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
