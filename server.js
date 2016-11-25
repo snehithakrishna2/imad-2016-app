abdddb547a42053b69538f3bc12c25f5377ecf1f
snehithakrishna2 <snehithakrishna7@gmail.com>
2016-11-24 21:43:11 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,13 +23,18 @@ app.get('/', function (req, res) {
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf","10000",salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input',function (req, res) {
   var hashedString=hash(req.params.input,'this-is-charmy');
   res.send(hashedString);
 });
+
+app.get('/create-user',funtion(req,res){
+    var salt =crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+ });
 
 
 var pool = new Pool(config);
