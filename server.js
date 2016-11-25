913ee840779ba9cc91b92d5e782ba2499cc54809
snehithakrishna2 <snehithakrishna7@gmail.com>
2016-11-24 22:50:46 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser=require('body-parser');
 
 var config = {
   user: 'snehithakrishna2',
@@ -15,7 +16,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -31,10 +32,21 @@ app.get('/hash/:input',function (req, res) {
   res.send(hashedString);
 });
 
-app.get('/create-user',funtion(req,res){
+app.post('/create-user',function(req,res) {
+    var username=req.body.username;
+    var password=req.body.password;
+    
     var salt =crypto.getRandomBytes(128).toString('hex');
     var dbString=hash(password,salt);
- });
+    
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result) {
+        if(err){
+           res.status(500).send(err.toString());
+        }else{
+           res.send('user successfully created' +username);
+         } 
+    });
+});
 
 
 var pool = new Pool(config);
