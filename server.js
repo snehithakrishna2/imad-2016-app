97c03be6c840554efa89fdb900dc6cfa8219eee1
snehithakrishna2 <snehithakrishna7@gmail.com>
2016-11-24 21:09:48 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
 var config = {
   user: 'snehithakrishna2',
   database:'snehithakrishna2',
@@ -10,17 +12,29 @@ var config = {
   password:process.env.DB_PASSWORD
 };
 
+
 var app = express();
 app.use(morgan('combined'));
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function (req, res) {
+  var hashedString=hash(req.params.input,'this-is-charmy');
+  res.send(hashedString);
+});
+
+
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
- //make a request
- // returns a response from a results
- pool.query('SELECT * FROM test',function(err,result){
+   pool.query('SELECT * FROM test',function(err,result){
  if(err){
      res.status(500).send(err.toString());
  }else{
@@ -28,6 +42,7 @@ app.get('/test-db', function (req, res) {
  }
    });
  });
+
 app.get('/about', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'about.html'));
 });
